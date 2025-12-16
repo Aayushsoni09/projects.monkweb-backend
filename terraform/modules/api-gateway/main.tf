84ad89d8822132fc9395347db560dc87cc082a7a
Aayush Soni <soniaayush24400@gmail.com>
2025-12-16 21:07:19 +0530

    added cors headers in api-gateway

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -1,12 +1,20 @@
 resource "aws_apigatewayv2_api" "this" {
   name          = "${var.project_name}-api"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["https://myresume.monkweb.tech"] 
+    allow_methods = ["POST", "OPTIONS"]
+    allow_headers = ["content-type"]
+    max_age       = 300
+  }
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
   api_id = aws_apigatewayv2_api.this.id
   integration_type = "AWS_PROXY"
   integration_uri  = var.lambda_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "post" {
